81ae5c6e6e41ba26b55c7f7e9dd2df9f39fa49a2
“Hira <hira.shrestha@avionte.com>
2026-01-25 04:57:59 +0545

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hira Shrestha on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
