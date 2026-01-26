e88ca09b5fd7aaa786b7722c66a60b48a83101ed
“Hira <hira.shrestha@avionte.com>
2026-01-26 14:25:00 +0545

    added sample data for daily scrum

---
+++ Scrumdinger/DailyScrum+extension.swift
@@ -0,0 +1,18 @@
+//
+//  DailyScrum+extension.swift
+//  Scrumdinger
+//
+//  Created by Hira Shrestha on 1/26/26.
+//
+
+import Foundation
+import ThemeKit
+
+extension DailyScrum {
+	static let sampleData: [DailyScrum] = [
+		DailyScrum(title: "Design", attendees: ["hira, Ram, Shyam"], lengthInminutes: 10, theme: .yellow),
+		DailyScrum(title: "App dev", attendees: ["hira1, Ram1, Shyam1"], lengthInminutes: 10, theme: .orange),
+		DailyScrum(title: "web dev", attendees: ["hira2, Ram2, Shyam2"], lengthInminutes: 10, theme: .poppy)
+
+		]
+}
---
+++ Scrumdinger/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Hira Shrestha on 1/26/26.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+	let title: String
+	let attendees: [String]
+	let lengthInminutes: Int
+	let theme: Theme
+}
